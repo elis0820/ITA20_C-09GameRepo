adb2134ba5015295f3a9a9ee2d43d60228f6fbe8
EI <77727564+elis0820@users.noreply.github.com>
2021-01-21 11:17:37 +0100

    Initialised console project
    using vscode

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITA20_C_09GameRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
